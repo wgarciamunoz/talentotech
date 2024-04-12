eb63e42a72006b541cd71dcc04f0ddd85b19247c
wgarciamunoz <88210706+wgarciamunoz@users.noreply.github.com>
2024-04-11 19:45:09 -0500

    Validación Formulario

--- js/main.js
+++ js/main.js
@@ -1,94 +1,79 @@
-class Persona {
-  // // propiedades de la clase Persona
-  // _nombre;
-  // _edad;
-  // _hijos;
-
-  // Constructor de la clase Persona
-  constructor(nombre, edad, peso, altura) {
-    this._nombre = nombre;
-    this._edad = edad;
-    this._hijos = []; // Agregar la variable de instancia hijos
-    this._peso = peso;
-    this._altura = altura;
+function validarNombre() {
+  var nombre = document.getElementById("nombre").value;
+  console.log(nombre);
+  if (nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
+    // Mostrar error
+    document.getElementById("errorNombre").style.display = "block";
+    // Aggregar clase de error
+    document.getElementById("errorNombre").classList.add("errorNombre");
+    // Mensaje de error
+    document.getElementById("errorNombre").innerHTML = "El campo nombre no puede estar vacío.";
+  } else {
+    // Ocultar error
+    document.getElementById("errorNombre").style.display = "none";
+    // Eliminar clase de error
+    document.getElementById("nombre").classList.remove("errorNombre");
   }
-
-  // Métodos de acceso para el nombre, la edad y los hijos
-  get nombre() {
-    return this._nombre;
-  }
-
-  set nombre(nombre) {
-    this._nombre = nombre;
-  }
-
-  get edad() {
-    return this._edad;
-  }
-
-  set edad(edad) {
-    this._edad = edad;
-  }
-
-  get hijos() {
-    return this._hijos;
-  }
-
-  set hijos(hijos) {
-    this._hijos = hijos;
-  }
-
-  get peso() {
-    return this._peso;
-  }
-
-  set peso(peso) {
-    this._peso = peso;
-  }
-
-  get altura() {
-    return this._altura;
-  }
- 
-  // Método para saludar
-  saludar() {
-    console.log(`Hola, mi nombre es ${this._nombre} y tengo ${this._edad} años y mis hijos son ${this._hijos}`);
-  }
-
-  // obtener IMC
-  calcularIMC() {
-    return this._peso / (this._altura * this._altura);
-  }
-
 
 }
 
+// validar teléfono
 
-class Jefe extends Persona {
-  constructor(nombre, edad, departamento) {
-    super(nombre, edad);
-    this._departamento = departamento;
-  }
+function validarTelefono() {
+  var telefono = document.getElementById("telefono").value;
+  console.log(telefono);
+  // validar formato empieza por 3 dígitos pero el primero debe ser un 3, seguido de un guión, seguido de 3 dígitos, seguido de un guión, seguido de 4 dígitos
+  var expresion = /^(3|6)\d{2}-\d{3}-\d{4}$/;
 
-  get departamento() {
-    return this._departamento;
-  }
+  if (!expresion.test(telefono) || telefono == null || telefono.length == 0) {
+    // Mostrar error
+    document.getElementById("errorTelefono").style.display = "block";
+    // Aggregar clase de error
+    document.getElementById("errorTelefono").classList.add("errorTelefono");
+    // Mensaje de error
+    document.getElementById("errorTelefono").innerHTML = "El formato del teléfono no es correcto.";
+  } else {  
+    // Ocultar error
+    document.getElementById("errorTelefono").style.display = "none";
+    // Eliminar clase de error
+    document.getElementById("telefono").classList.remove("errorTelefono");
+  } 
 
-  set departamento(departamento) {
-    this._departamento = departamento;
-  }
-
-  saludar() {
-    console.log(`Hola, mi nombre es ${this._nombre}, tengo ${this._edad} años y soy jefe del departamento de ${this._departamento}`);
-  }
 }
 
-Geova = new Persona('Geova', 25, 1.73, 70);
-Geova.saludar();
-console.log(Geova.calcularIMC());
+// validar correo
 
-Geova._hijos.push('Juan', 'Pedro', 'María');
+function validarCorreo() {
+  var correo = document.getElementById("correo").value;
+  console.log(correo);
+  // validar formato de correo
+  var expresion = /\w+@\w+\.+[a-z]/;
 
+  if (!expresion.test(correo) || correo == null || correo.length == 0) {
+    // Mostrar error
+    document.getElementById("errorCorreo").style.display = "block";
+    // Aggregar clase de error
+    document.getElementById("errorCorreo").classList.add("errorCorreo");
+    // Mensaje de error
+    document.getElementById("errorCorreo").innerHTML = "El formato del correo no es correcto.";
+  } else {  
+    // Ocultar error
+    document.getElementById("errorCorreo").style.display = "none";
+    // Eliminar clase de error
+    document.getElementById("correo").classList.remove("errorCorreo");
+  } 
 
-Jefe = new Jefe('Geova', 25, 'Desarrollo');
-Jefe.saludar(); // Hola, mi nombre es Geova, tengo 25 años y soy jefe del departamento de Desarrollo
+}
+
+document.getElementById("nombre").addEventListener("blur", validarNombre);
+document.getElementById("telefono").addEventListener("blur", validarTelefono);
+document.getElementById("correo").addEventListener("blur", validarCorreo);
+
+// Evento click del botón enviar formulario
+function enviarFormulario() {
+  validarNombre();
+  validarTelefono();
+  validarCorreo();
+}
+
+document.getElementById("enviar").addEventListener("click", enviarFormulario);
